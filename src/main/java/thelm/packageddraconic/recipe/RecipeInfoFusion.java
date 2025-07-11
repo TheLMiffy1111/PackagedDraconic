8ce928e6f0d90754f596a4bb409a27c10d843a45
TheLMiffy1111 <6522061+TheLMiffy1111@users.noreply.github.com>
2025-07-12 06:04:57 +0800

    Fix encoder loading of fusion recipes with more than 27 injector inputs

--- src/main/java/thelm/packageddraconic/recipe/RecipeInfoFusion.java
+++ src/main/java/thelm/packageddraconic/recipe/RecipeInfoFusion.java
@@ -185,7 +185,7 @@ public class RecipeInfoFusion implements IRecipeInfoFusion {
 	public Int2ObjectMap<ItemStack> getEncoderStacks() {
 		Int2ObjectMap<ItemStack> map = new Int2ObjectOpenHashMap<>();
 		int[] slotArray = RecipeTypeFusion.SLOTS.toIntArray();
-		ArrayUtils.remove(slotArray, 27);
+		slotArray = ArrayUtils.remove(slotArray, 27);
 		map.put(40, inputCore);
 		for(int i = 0; i < inputInjector.size(); ++i) {
 			map.put(slotArray[i], inputInjector.get(i));
